0d13f849af9b31f39d858e1a4dedeeefc1961ee6
gauravtaulkar <119947138+gauravtaulkar@users.noreply.github.com>
2024-05-04 13:26:59 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+//        load sample.fxml
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("sample.fxml")));
+        primaryStage.setTitle("MyVideoPlayer");
+        primaryStage.setScene(new Scene(root, 800, 650));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
